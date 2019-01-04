6a6aef47bd7f5e72fb7d06f5a1c60fdc4d021c66
Mithunraj <mithun@gnts.in>
2019-01-04 17:39:42 +0530

    Change: Add Edit Customer screen->Business details, Payments section(UI changes)

--- src/app/business/customer/add-edit-customer/customer-business-details/customer-business-details.component.ts
+++ src/app/business/customer/add-edit-customer/customer-business-details/customer-business-details.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-business-details.component.scss']
 })
 export class CustomerBusinessDetailsComponent implements OnInit {
+  formTitle: string;
+  formSubmit: string;
+  socialidPage: boolean;
+  socialiddtls: {};
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  addSocialId() {
+    this.socialiddtls = {};
+    this.formTitle = 'Add Social Id';
+    this.formSubmit = 'Save';
+    this.openMyModal('socialidmodal');
+    this.socialidPage = false;
+  }
+  editSocialId(data) {
+    this.socialiddtls = data;
+    this.formTitle = 'Edit Social Id';
+    this.formSubmit = 'Update';
+    this.openMyModal('socialidmodal');
+    this.socialidPage = true;
+  }
+  openMyModal(event) {
+    document.querySelector('#' + event).classList.add('md-show');
+  }
+  closeMyModal(event) {
+    ((event.target.parentElement.parentElement).parentElement).classList.remove('md-show');
+  }
 }
