150dc215c5116f4d72b0e78cf10632412dca6c51
Mithunraj <mithun@gnts.in>
2019-01-12 12:09:22 +0530

    Consumer-> Tab implementation; Dealer-> Payment section, Customer section-> Header alignment

--- src/app/business/consumer/view-consumer/view-consumer.component.ts
+++ src/app/business/consumer/view-consumer/view-consumer.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConstant } from '../../../app.constants';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-view-consumer',
   templateUrl: './view-consumer.component.html',
-  styleUrls: ['./view-consumer.component.scss']
+  styleUrls: ['./view-consumer.component.scss'],
+  animations: [
+    trigger('fadeInOutTranslate', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('400ms ease-in-out', style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        style({transform: 'translate(0)'}),
+        animate('400ms ease-in-out', style({opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class ViewConsumerComponent implements OnInit {
   date_displayformat = AppConstant.API_CONFIG.ANG_DATE.displaydtime;
