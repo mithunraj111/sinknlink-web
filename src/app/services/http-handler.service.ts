242c4caf9030093a48723453965be47b166f77b0
Vidhya M <vidhya.m@aaludra.com>
2019-04-08 14:58:47 +0530

    #205 - Token issue fixed

--- src/app/services/http-handler.service.ts
+++ src/app/services/http-handler.service.ts
@@ -9,33 +9,50 @@ export class HttpHandlerService {
     userstoragedata: any;
     headers: any;
     constructor(private http: Http, private localStorageService: LocalStorageService) {
-        this.userstoragedata = this.localStorageService.getItem(AppConstant.LOCALSTORAGE.USER);
-        this.headers = new Headers();
-        if (this.userstoragedata) {
-            this.headers.append('Authorization',
-                this.userstoragedata.token);
-        }
     }
 
     GET(url: string, options?): Observable<any> {
-        return this.http.get(url, { headers: this.headers });
+        this.userstoragedata = this.localStorageService.getItem(AppConstant.LOCALSTORAGE.USER);
+        this.headers = new Headers();
+        if (this.userstoragedata != null) {
+            this.headers.append('Authorization',
+                this.userstoragedata.token);
+            return this.http.get(url, { headers: this.headers });
+        }
     }
 
     POST(url: string, data: any, options?: any): Observable<any> {
         if (data.auth) {
             return this.http.post(url, data);
         } else {
-            return this.http.post(url, data, { headers: this.headers });
+            this.userstoragedata = this.localStorageService.getItem(AppConstant.LOCALSTORAGE.USER);
+            this.headers = new Headers();
+            if (this.userstoragedata != null) {
+                this.headers.append('Authorization',
+                    this.userstoragedata.token);
+                return this.http.post(url, data, { headers: this.headers });
+            }
         }
     }
 
     PUT(url: string, data: any, id: number, options?: any): Observable<any> {
         url = url + '/' + id;
-        return this.http.put(url, data, { headers: this.headers });
+        this.userstoragedata = this.localStorageService.getItem(AppConstant.LOCALSTORAGE.USER);
+        if (this.userstoragedata != null) {
+            this.headers.append('Authorization',
+            this.userstoragedata.token);
+            return this.http.put(url, data, { headers: this.headers });
+        }
     }
 
     DELETE(url: string, id: number, data?: any, options?: any): Observable<any> {
         url = url + id;
-        return this.http.delete(url, { headers: this.headers });
+        this.userstoragedata = this.localStorageService.getItem(AppConstant.LOCALSTORAGE.USER);
+        this.headers = new Headers();
+        if (this.userstoragedata != null) {
+            this.headers.append('Authorization',
+            this.userstoragedata.token);
+            return this.http.delete(url, { headers: this.headers });
+        }
     }
 }
